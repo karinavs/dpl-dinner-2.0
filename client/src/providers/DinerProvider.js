63aef2da973cff3b7d85d636492cb50428dc964d
Karina Stevens <keva86@hotmail.com>
2020-06-25 21:01:22 -0600

    MenuProvider ItemProvider

--- client/src/providers/DinerProvider.js
+++ client/src/providers/DinerProvider.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const DinerContext = React.createContext();
+export const DinerConsumer = DinerContext.Consumer;
+class DinerProvider extends Component {
+  state = { diners: [] }
+  componentDidMount() {
+    axios.get('/api/diners')
+      .then( res => {
+        this.setState({ diners: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  addDiner = (diner) => {
+    axios.post('/api/diners', { diner } )
+      .then( res => {
+        const { diners } = this.state
+        // const diners = this.state.diners 
+        this.setState({ diners: [...diners, res.data]})
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  updateDiner = (id, diner) => {
+    axios.put(`/api/diners/${id}`, { diner })
+      .then( res => {
+        const diners = this.state.diners.map( d => {
+          if (d.id === id) {
+            return res.data
+          }
+          return d
+        })
+        this.setState({ diners })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  deleteDiner = (id) => {
+    axios.delete(`/api/diners/${id}`)
+      .then( res => {
+        const { diners } = this.state
+        this.setState({ diners: diners.filter( d => d.id !== id )})
+      })
+  }
+
+  render() {
+    return(
+      <DinerContext.Provider value={{
+        ...this.state,
+        addDiner: this.addDiner,
+        updateDiner: this.updateDiner,
+        deleteDiner: this.deleteDiner,
+      }}>
+        { this.props.children }
+      </DinerContext.Provider>
+    )
+  }
+}
+
+export default DinerProvider;
